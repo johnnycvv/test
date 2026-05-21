b1c2009d5a737899aac12dac8684f091a616e993
Jake <stoiccity@adexec.com>
2026-05-21 02:03:27 +0100

    Remove paywall completely

--- ui/src/app/page.js
+++ ui/src/app/page.js
@@ -3,8 +3,6 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 export default function Home() {
   const router = useRouter();
-  useEffect(() => {
-    router.replace('/login');
-  }, [router]);
+  useEffect(() => { router.replace('/login'); }, []);
   return null;
 }
